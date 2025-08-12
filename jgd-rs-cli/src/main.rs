7a1c59809cd025e002dd41a540c600f878097634
Luis Vendrame <lvendrame@gmail.com>
2025-08-12 20:03:03 +0100

    chore: reorg shema and examples

--- jgd-rs-cli/src/main.rs
+++ jgd-rs-cli/src/main.rs
@@ -103,10 +103,11 @@ fn open_and_print(path: &str) {
 }
 
 fn main() -> Result<()> {
-    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/jgd-lib/examples/single-object-root.jgd");
-    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/jgd-lib/examples/array-object-root.jgd");
-    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/jgd-lib/examples/ranged-array-object-root.jgd");
-    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/jgd-lib/examples/user-post-entities.jgd");
+    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/examples/single-object-root.jgd");
+    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/examples/array-object-root.jgd");
+    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/examples/ranged-array-object-root.jgd");
+    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/examples/user-post-entities.jgd");
+    open_and_print("/Users/lvendrame/projects/pocs/jgd-rs/examples/customers-orders.jgd");
     return Ok(());
 
     let cli = Cli::parse();
