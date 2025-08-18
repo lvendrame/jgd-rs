96ae6fdbee2d4bc6404a46d625ef7f3b4e6824bc
Luis Vendrame <lvendrame@gmail.com>
2025-08-18 18:37:16 +0100

    refactor: fix global_config file name

--- jgd-rs/src/type_spec/utils/mod.rs
+++ jgd-rs/src/type_spec/utils/mod.rs
@@ -2,14 +2,12 @@ mod generator_config;
 mod local_config;
 mod replacer;
 mod arguments;
-mod jdg_global_config;
+mod jgd_global_config;
 mod jgd_generator_error;
-mod key_parser;
 
 pub use generator_config::*;
 pub use replacer::*;
 pub use arguments::*;
-pub use jdg_global_config::*;
+pub use jgd_global_config::*;
 pub use jgd_generator_error::*;
 pub use local_config::*;
-pub use key_parser::*;
